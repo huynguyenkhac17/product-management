92daf77aa2486ed2f8d96d49e56241503ad14680
huynguyenkhac17 <23001525@hus.edu.vn>
2026-05-13 06:22:09 +0700

    fix: annotation default & exclude mongodb on codebase

--- src/main/java/com/example/product/App.java
+++ src/main/java/com/example/product/App.java
@@ -2,6 +2,7 @@ package com.example.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,7 +11,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class App {
 
